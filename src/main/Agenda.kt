e1e474a6ae2299289333454b3828e9f828c840fa
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2020-01-14 10:33:11 -0300

    03.01_Tipos_Basicos_Kotlin

--- src/main/Agenda.kt
+++ src/main/Agenda.kt
@@ -3,4 +3,18 @@ package main
 fun main() {
     println("Olá, mundo!!")
     println("Curso de Kotlin da TreinaWeb");
+
+    var nomeDaVariavel : Int = 10 // Tipo Declarado
+    var nomeDaVariavel2 = 20 // Tipo Inferido
+    var nomeDaVariavel3 : String = "TreinaWeb"
+    var nomeDaVariavel4 = "TreinaWeb"
+    var nomeDaVariavel5 : Double = 5.5
+    var nomeDaVariavel6 = 5.5
+    var nomeDaVariavel7 : Float = 5.5f
+    var nomeDaVariavel8 = 5.5f
+    nomeDaVariavel8 = 10.0f
+
+    println(nomeDaVariavel4)
+
+
 }
