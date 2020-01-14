7207b38a6c2c34fd44233899b3dd38ee63d9f43a
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2020-01-14 11:00:47 -0300

    03.04_Conversoes_Kotlin

--- src/main/Agenda.kt
+++ src/main/Agenda.kt
@@ -8,17 +8,23 @@ fun main() {
     val emailContato = "joao@mail.com"
     val idadeContato : Int = 30
 
-//    var nomeDaVariavel : Int = 10 // Tipo Declarado
+    var nomeDaVariavel : Int = 10 // Tipo Declarado
 //    var nomeDaVariavel2 = 20 // Tipo Inferido
 //    val nomeDaVariavel3 : String
 //    val nomeDaVariavel4 = "TreinaWeb"
-//    var nomeDaVariavel5 : Double = 5.5
+    var nomeDaVariavel5 : Double = 5.5
 //    var nomeDaVariavel6 = 5.5
 //    var nomeDaVariavel7 : Float = 5.5f
 //    var nomeDaVariavel8 = 5.5f
 //    nomeDaVariavel8 = 10.0f
 //
 //    nomeDaVariavel3 = "Teste"
+
+    println(nomeDaVariavel5.toFloat() is Float)
+
+    println(nomeDaVariavel.toString() is String)
+
+    println(nomeDaVariavel)
 
 
     println(nomeContato)
