dc2073a4601d0867bd821ace2163e1b798e3d237
Fagner Pinheiro <fagnerpinheirosantos@gmail.com>
2020-01-21 10:10:49 -0300

    10.02_Criando_Listas

--- src/main/Agenda.kt
+++ src/main/Agenda.kt
@@ -8,6 +8,7 @@ fun main() {
     println("Olá, mundo!!")
     println("Curso de Kotlin da TreinaWeb");
     var opcao: Int? = 0
+    var listaContato = mutableListOf<Contato>()
     nome_do_loop@ do {
         var nomeContato: String? = null // Safe Call Operator
         val emailContato: String
@@ -20,6 +21,7 @@ fun main() {
         idadeContato = readLine()?.toInt()
 
         var contato1 = Contato(nomeContato, emailContato, idadeContato)
+        listaContato.add(contato1) // ((Maria, maria@mail.com, 50), (João, joao@mail.com, 30))
 
         println("O nome do contato é: ${contato1.nomeContato} que possui ${contato1.nomeContato?.length} caracteres")
         println("O email do contato é: ${contato1.emailContato}")
@@ -47,14 +49,18 @@ fun main() {
 //            println(i)
 //        }
 
-        println(contato1.nomeContato.equals("Maria"))
-        println(contato1.nomeContato?.reversed())
-        println(contato1.nomeContato?.slice(1..3))
-        println(contato1.nomeContato?.get(3))
+//        println(contato1.nomeContato.equals("Maria"))
+//        println(contato1.nomeContato?.reversed())
+//        println(contato1.nomeContato?.slice(1..3))
+//        println(contato1.nomeContato?.get(3))
 
         println("Deseja continuar? 0 - SIM / 1 - NÃO")
         opcao = readLine()?.toInt()
     } while(opcao == 0)
+
+    for (i in listaContato) {
+        println("Nome: ${i.nomeContato} / Idade: ${i.idadeContato} / Email: ${i.emailContato}")
+    }
 
     println("Obrigado por utilizar nosso gerenciamento de contatos")
 
